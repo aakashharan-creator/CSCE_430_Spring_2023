28ba31d3482108b3f76233a5d73bde796d0a1893
Aakash Haran <aakash.haran01@gmail.com>
2023-03-29 14:50:50 +0000

    ran verifyproblem problem2html problem2pdf

--- jamesstreets/input_validators/validate.py
+++ jamesstreets/input_validators/validate.py
@@ -11,16 +11,16 @@ assert (n.isdigit() and k.isdigit()) and (int(n) > 0 and int(k) > 0), "%s, %s gi
 n = int(n)
 k = int(k)
 
-assert 1 <= n <= 100_000, "%s  not in [1, 100_000]" % (n)
-assert 1 <= k <= 10_000, "%s  not in [1, 10_000]" % (k)
+assert 1 <= n <= 100000, "%s  not in [1, 100_000]" % (n)
+assert 1 <= k <= 50000, "%s  not in [1, 50_000]" % (k)
 
 cars = stdin.readline().strip().split()
 
 assert len(cars) == n, "num cars %s not equal to %s" % (len(cars), n)
 
 for c in cars:
-    assert c.lstrip("-").isdigit(), "%s is not an integer", c
+    assert c.lstrip("-").isdigit(), "%s is not an integer" % c
     c = int(c)
-    assert -1_000_000 <= c <= 1_000_000, "%s is not in the range [-1_000_000, 1_000_000]" % c
+    assert -1000000 <= c <= 1000000, "%s is not in the range [-1_000_000, 1_000_000]" % c
 
 exit(42)
