df71878b4a6286f95f109df42433e5558708a7dd
Aakash Haran <aakash.haran01@gmail.com>
2023-03-29 06:08:09 +0000

    test

--- submissions/accepted/accepted.py
+++ submissions/accepted/accepted.py
@@ -10,7 +10,7 @@ right = sorted([car for car in cars if car > 0])
 curr_time = 0
 left_index = 0
 right_index = 0
-MAX_TIME = 1_000_000 + k
+MAX_TIME = 1000000 + k
 while left_index < len(left) or right_index < len(right):
     left_car = MAX_TIME if left_index >= len(left) else left[left_index]
     right_car = MAX_TIME if right_index >= len(right) else right[right_index]
